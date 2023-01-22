2c30333c9a83addc52db6c221bed2ca64ab4b20e
JHammersley <104778381+JHammersley@users.noreply.github.com>
2023-01-21 23:08:00 -0700

    added

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -39,7 +39,7 @@ debugger;
         this.editorCreate();
 
 			// jason
-			this.jason = this.physics.add.sprite(170, 266, "jason", "jason0");
+			this.jason = this.physics.add.sprite(170, 266, "jason", 0);
 			this.jason.scaleX = 3;
 			this.jason.scaleY = 3;
 			this.jason.body.setSize(16, 16, false);
@@ -50,45 +50,45 @@ debugger;
 			// Create animations from the jason spritesheet
 			this.anims.create({
 				key: "jason-walk-up",
-				frames: this.anims.generateFrameNames("jason", {prefix:'jason', start: 9, end: 11}),
+				frames: this.anims.generateFrameNames("jason", {start: 9, end: 11, zeroPad: 2, prefix: "jason"}),
 				frameRate: 10,
 				repeat: -1
 			});
 
 			this.anims.create({
 				key: "jason-walk-left",
-				frames: this.anims.generateFrameNames("jason", {prefix:'jason', start: 3, end: 5}),
+				frames: this.anims.generateFrameNames("jason", {start: 3, end: 5, zeroPad: 2, prefix: "jason"}),
 				frameRate: 10,
 				repeat: -1
 			});
 
 			this.anims.create({
 				key: "jason-walk-right",
-				frames: this.anims.generateFrameNames("jason", {prefix:'jason', start: 6, end: 8}),
+				frames: this.anims.generateFrameNames("jason", {start: 6, end: 8, zeroPad: 2, prefix: "jason"}),
 				frameRate: 10,
 				repeat: -1
 			});
 
 			this.anims.create({
 				key: "jason-walk-down",
-				frames: this.anims.generateFrameNames("jason", {prefix:'jason', start: 0, end: 2}),
+				frames: this.anims.generateFrameNames("jason", {start: 0, end: 2, zeroPad: 2, prefix: "jason"}),
 				frameRate: 10,
 				repeat: -1
 			});
 
 			this.anims.create({
 				key: "jason-idle-down",
-				frames: [{ key: "jason", frame: 0 }],
+				frames: [{ key: "jason", frame: "jason00" }],
 				repeat: -1
 			});
 
-			//this.jason.anims.play("jason-idle-down");
+						//this.jason.anims.play("jason-idle-down");
 
-            //this.setFrame(this.idleFrame.down)
+						//this.setFrame(this.idleFrame.down)
 
-            // Set up player input
-        
-    }
+						// Set up player input
+					
+				}
 
     update() {
 		if (this.cursors.up.isDown) {
